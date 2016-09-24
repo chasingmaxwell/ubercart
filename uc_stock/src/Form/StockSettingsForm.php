132b454254c894785ec16cc84569a259371b930a
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-09-24 14:01:05 -0700

    Issue #2800513 by TR: Fix token tree browser theme callback

--- uc_stock/src/Form/StockSettingsForm.php
+++ uc_stock/src/Form/StockSettingsForm.php
@@ -62,9 +62,9 @@ class StockSettingsForm extends ConfigFormBase {
     );
 
     if (\Drupal::moduleHandler()->moduleExists('token')) {
-      module_load_include('inc', 'token', 'token.pages');
       $form['token_tree'] = array(
-        '#markup' => theme_token_tree(array('token_types' => array('uc_order', 'uc_stock', 'node', 'site', 'store'))),
+        '#theme' => 'token_tree_link',
+        '#token_types' => ['uc_order', 'uc_stock', 'node', 'site', 'store'],
       );
     }
 
