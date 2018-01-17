003f338d3df44df76ba6175f3d4b0755c21616b0
Tim Rohaly <tr@202830.no-reply.drupal.org>
2018-01-17 01:51:53 -0800

    Create a base class for Order rules tests - will be used by event tests, condition tests, and action tests. #2924148-59

--- uc_order/tests/src/Functional/OrderRulesEventsTest.php
+++ uc_order/tests/src/Functional/OrderRulesEventsTest.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\Tests\uc_order\Functional;
 
-use Drupal\Tests\BrowserTestBase;
 use Drupal\rules\Context\ContextConfig;
-use Drupal\Tests\uc_order\Traits\OrderTestTrait;
 
 /**
  * Tests the four events that uc_order provides for use in Rules module.
@@ -14,73 +12,7 @@ use Drupal\Tests\uc_order\Traits\OrderTestTrait;
  * @todo Remove the 'legacy' tag when Rules no longer uses deprecated code.
  * @see https://www.drupal.org/project/rules/issues/2922757
  */
-class OrderRulesEventsTest extends BrowserTestBase {
-  use OrderTestTrait;
-
-  /**
-   * The profile to install as a basis for testing.
-   *
-   * @var string
-   */
-  protected $profile = 'testing';
-
-  /**
-   * Additional modules required.
-   *
-   * @var string[]
-   */
-  public static $modules = ['uc_order', 'rules'];
-
-  /**
-   * Don't check for or validate config schema.
-   *
-   * @var bool
-   */
-  protected $strictConfigSchema = FALSE;
-
-  /**
-   * A user with administration rights.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected $adminUser;
-
-  /**
-   * Reaction Rules entity storage.
-   *
-   * @var \Drupal\rules\Entity\ReactionRuleStorage
-   */
-  protected $rulesStorage;
-
-  /**
-   * The Rules expression manager.
-   *
-   * @var \Drupal\rules\Engine\ExpressionManager
-   */
-  protected $expressionManager;
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-
-    $this->adminUser = $this->drupalCreateUser([
-      'access administration pages',
-      'access content',
-      'view all orders',
-      'delete orders',
-      'unconditionally delete orders',
-      'edit orders',
-    ]);
-
-    // All of the events we're testing are or can be initiated
-    // by an administrator's actions.
-    $this->drupalLogin($this->adminUser);
-
-    $this->rulesStorage = $this->container->get('entity_type.manager')->getStorage('rules_reaction_rule');
-    $this->expressionManager = $this->container->get('plugin.manager.rules_expression');
-  }
+class OrderRulesEventsTest extends OrderRulesTestBase {
 
   /**
    * Tests the four events provided by uc_order.
