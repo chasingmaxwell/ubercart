15974a39d8127daf164fd906a54879b4f11e4aab
Peter Sieg <chasingmaxwell@gmail.com>
2019-04-03 23:18:57 -0400

    fix: merge override config recursively

--- payment/uc_paypal_checkout/js/paypal-button.js
+++ payment/uc_paypal_checkout/js/paypal-button.js
@@ -8,17 +8,18 @@
   Drupal.behaviors.ucPaypalCheckout = {
     attach: function (context, settings) {
       $('#paypal-button').once().each(() => {
-        var config = settings.ucPaypalCheckout.overrideConfig || {};
+        var config = {};
 
         // Configuration
-        config.env = config.env || settings.ucPaypalCheckout.env;
-        config.locale = config.locale || settings.ucPaypalCheckout.locale;
-        config.style = config.style || settings.ucPaypalCheckout.buttonStyle;
-        config.commit = config.commit || true;
-        config.funding = config.funding || {};
-        config.funding.allowed = config.funding.allowed || settings.ucPaypalCheckout.allowedFunding.map(function (source) { return paypal.FUNDING[source]; });
+        config.env = settings.ucPaypalCheckout.env;
+        config.locale = settings.ucPaypalCheckout.locale;
+        config.style = settings.ucPaypalCheckout.buttonStyle;
+        config.commit = true;
+        config.funding = {};
+        config.funding.allowed = settings.ucPaypalCheckout.allowedFunding.map(function (source) { return paypal.FUNDING[source]; });
 
-        console.log('***************'.repeat(9), config);
+        // Merge with config override
+        config = $.extend(true, config, settings.ucPaypalCheckout.overrideConfig || {});
 
         // Set up a payment
         config.payment = function (data, actions) {
